c182a662b7f8dc9ea155cea09066fe547e316168
zi-min-pan <79899976+zi-min-pan@users.noreply.github.com>
2021-06-13 00:00:41 +0800

    Update
    新增幾項功能的data field 和 function
    包括 剩餘錯誤次數 增加確保唯一解的函式的效率 輸贏判別

--- Sudoku.h
+++ Sudoku.h
@@ -1,17 +1,18 @@
-#pragma once
+#ifndef SUDOKU_H
+#define SUDOKU_H
 class Sudoku {
 public:
 	Sudoku();
 	bool creat();
-	void print();
-	void start();
-	
-
+	void print(int, int);
+	void start(int, int);
 private:
 	int data[81];
 	int mask[81];
+	int exit[81];
+	int life = 5;
 	void createMask();
-	void input(int, int, int);
+	bool input(int, int, int, int, int);
 	bool isRowVailable(int*);
 	bool isColumnVailable(int*);
 	bool isBoxVailable(int, int,int[]);
@@ -19,6 +20,11 @@ private:
 	void copy(const int*, int*);
 	void dig();
 	int ans;
-	int* ansList[2];
+	int createMaskTimes = 0;
 	int digData[81];
+	void setColor(int color = 7);
+	void setPosition(int, int);
+	void printLife();
 };
+
+#endif // !SUDOKU_H
