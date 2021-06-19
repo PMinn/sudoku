d10e601465d4029857da7f7d989e46c318b46953
zi-min-pan <79899976+zi-min-pan@users.noreply.github.com>
2021-06-20 00:00:27 +0800

    Update
    新增init()
    新增callback function做載入畫面
    新增operator=

--- Sudoku.h
+++ Sudoku.h
@@ -1,26 +1,31 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
+typedef void(*callback)(int, int, int);
 class Sudoku {
 public:
 	Sudoku();
-	bool creat();
+	bool creat(callback);
 	void print(int, int);
-	void start(int, int);
+	bool start(int, int);
+	void clear();
+	void init();
+	Sudoku& operator=(Sudoku&);
 private:
 	int data[81];
 	int mask[81];
 	int exit[81];
 	int life = 5;
-	void createMask();
+	void createMask(callback);
 	bool input(int, int, int, int, int);
 	bool isRowVailable(int*);
 	bool isColumnVailable(int*);
 	bool isBoxVailable(int, int,int[]);
 	bool getAnswer(int*);
 	void copy(const int*, int*);
-	void dig();
+	void dig(callback);
 	int ans;
-	int createMaskTimes = 0;
+	int creatTimes;
+	int createMaskTimes;
 	int digData[81];
 	void setColor(int color = 7);
 	void setPosition(int, int);
